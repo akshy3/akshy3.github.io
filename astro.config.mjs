3a65968a918ce539fd5b6d6bbc0f89c514558c7a
Akshay <akshaymashokan@gmail.com>
2024-04-17 18:23:24 +0530

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+import tailwind from "@astrojs/tailwind";
+import compress from "astro-compress";
+import icon from "astro-icon";
+import { defineConfig } from 'astro/config';
+
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://akshy3.github.io",
+  integrations: [tailwind(), icon(), compress()],
+  output: "server",
+  adapter: vercel()
+});
